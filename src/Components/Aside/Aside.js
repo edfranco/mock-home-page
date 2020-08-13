0e3b7258933841f3092f029903f3553293c9d3b6
System Administrator <root@gfx-oncallmbp3.lan>
2020-08-13 11:27:04 -0700

    created constant variables to pull from

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import {visitAsideLinks as LINKS} from '../../constants/asideLinks/asideLinks';
+
 class Aside extends React.Component{
+    state={
+        asideContent: {
+            header: 'Visit',
+            links: LINKS,
+        }
+    }
+
+    displayAside = () =>{
+        let elements = [];
+        this.state.asideContent.links.forEach(link=>{
+            elements.push(
+                <a href={link.href}>{link.text}</a>
+            );
+        });
+        return elements;
+    }
+    
     render(){
         return(
             <div className="aside">
-
+                <a href="/visit"><h4>Visit</h4></a>
+                {this.displayAside()}
             </div>
         );
     };
--- src/containers/VisitContainer/VisitContainer.js
+++ src/containers/VisitContainer/VisitContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import VisitRoutes from '../../config/VisitRoutes';
+import Aside from '../../Components/Aside/Aside';   
 
 import './VisitContainer.css';
 
@@ -13,38 +14,8 @@ class VisitContainer extends React.Component{
                 <div className="visit-content">
                 <div className="visit-aside">
                 <div className="aside-links">
-                        <a href="/visit"><h4>Visit</h4></a>
-                        <a href="./visit/tickets">Buy Tickets</a>
-                        <a>Frequently Asked Questions</a>
-                        <a>Calendar</a>
-                        <a>Hours</a>
-                        <a>Getting Here</a>
-                        <a>Museum Map</a>
-                        <a>Reduced Rates & Community Day</a>
-                        <a>Accessibility</a>
-                        <a>Tips for Visiting with Kids</a>
-                        <a>Exhibits</a>
-                        <a>Tactile Dome</a>
-                        <a>Artworks on View</a>
-                        <a>Cinema Arts</a>
-                        <a>Kanbar Forum</a>
-                        <a>Black Box</a>
-                        <a>Museum Galleries</a>
-                        <a>Restaurant & Café</a>
-                        <a>School Field Trips</a>
-                        <a>Groups / Tour Operators</a>
-                        <a>Event Rentals</a>
-                        <a>Exploratorium Store</a>
-                        <a>Contact Us</a>
-                        <a>Español</a>
-                        <a>繁體中文</a>
-                        <a>简体中文</a>
-                        <a>한국어</a>
-                        <a>Français</a>
-                        <a>Deutsch</a>
-                        <a>Português</a>
-                        <a>日本語</a>
-                    </div>
+                    <Aside />
+                </div>
                 </div>
                 <VisitRoutes />
                 </div>
